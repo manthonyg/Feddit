c977daa29826a7c56edccac7041c686f6b40ab52
manthony <41161951+manthonyg@users.noreply.github.com>
2021-03-09 11:22:44 -0500

    created controller layer for posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Post = require('../models/post');
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
-const { updateNamespaceExportDeclaration } = require("typescript");
+const PostsController = require("../controllers/posts.controller");
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -32,169 +31,43 @@ const storageLocation = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storageLocation })
+const upload = multer({ storage: storageLocation });
+
 /** 
  * @description Create a new post
  */
-router.post('/posts', checkAuth, upload.single("image"), async (req, res, next) => {
-
-  const url = req.protocol + '://' + req.get("host");
-
-    const post = new Post({
-      title: req.body.title,
-      message: req.body.message,
-      imagePath: url + '/images/' + req.file.filename,
-      creator: req.userData.userId
-  });
-  try {
-    const savedPost = await post.save()
-    res
-    .status(201)
-    .json(savedPost)
-  }
-  catch(error) {
-    res
-    .status(404)
-    .json({
-      error: 'Could not create post'
-    })
-  }
-});
+router.post('/posts', checkAuth, upload.single("image"), PostsController.createPost);
 
 
 /**
  * @description Get all posts count
  */
-router.get('/posts/count', async (req, res, next) => {
-
-  try {
-    const postCount = await Post.count();
-    res
-    .status(200)
-    .json(postCount)
-  }
-  catch(error) {
-    res
-    .status(404)
-    .json({
-      error: 'Could not get post count'
-    })
-  }
-});
+router.get('/posts/count',  PostsController.getPostCount);
 
 
 /**
  * @description Get all posts within pagination range in query params
  */
-router.get('/posts', async (req, res, next) => {
+router.get('/posts', PostsController.getPosts); 
 
-  const pageSize = +req.query.pagesize;
-  const currentPage = +req.query.page;
-  const postQuery = Post.find().limit(5);
-
-  if (pageSize && currentPage) {   
-    postQuery
-    .skip(pageSize * (currentPage - 1))
-    .limit(+pageSize);
-  }
-
-  try {
-    const posts = await postQuery;
-
-    res
-    .status(200)
-    .json(posts);
-  }
-  catch(error) {
-    res
-    .status(404)
-    .json({
-      error: 'Could not get posts'
-    })
-  }
-});
 
 /**
  * @description
  * Update single post
  */
-router.put('/posts/:postId', checkAuth, upload.single("image"), async (req, res, next) => {
-    
-  const url = req.protocol + '://' + req.get("host");
-  let imagePath = req.body.imagePath;
-
-  if (req.file) {
-    // multer found an image File
-      imagePath = url + '/images/' + req.file.filename
-  }
-
-    updatedPost = {
-      title: req.body.title,
-      message: req.body.message,
-      imagePath: imagePath
-    }
-    
-    try {
-      await Post.updateOne({_id: req.params.postId, creator: req.userData.userId}, updatedPost)
-      res
-      .status(200)
-      .json(updatedPost)
-    }
-    catch(error) {
-      res
-      .status(404)
-      .json({
-        error: 'Could not update post'
-      })
-    }
-});
+router.put('/posts/:postId', checkAuth, upload.single("image"), PostsController.updatePost);
 
 /**
  * @description
  * Get single post
  */
-router.get('/posts/:postId', async (req, res, next) => {
-  try {
-    const postId = req.params.postId
-    const post = await Post.findById({_id: postId})
-    res
-    .status(200)
-    .json(post);
-  }
-  catch(error) {
-    res
-    .status(404)
-    .json({
-      error: 'Could not get post'
-    })
-  }
-});
+router.get('/posts/:postId', PostsController.getPost);
 
 /**
  * @description
  * Delete single post
  */
-router.delete('/posts', checkAuth, async (req, res, next) => {
-  const targetId = req.body._id
-
-  try {
-    await Post.deleteOne({_id: targetId, creator: req.userData.userId})
-    res
-    .status(200)
-    .json({
-      message: 'Successfully deleted'
-    });
-  }
-
-  catch(error) {
-    res
-    .status(404)
-    .json({
-      error: 'Could not delete'
-    })
-    
-  }
-});
+router.delete('/posts', checkAuth, PostsController.deletePost); 
 
 
 module.exports = router
